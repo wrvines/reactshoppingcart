b0a2e7247395769ec006547aac5800bac57aafb7
poopysmack <54606838+poopysmack@users.noreply.github.com>
2022-11-07 09:18:15 -0700

    added css to products

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,9 +6,9 @@ function Product(props) {
   return (
     <Link to={`/products/${props.id}`}> 
     <div className='products'>
-        <h3>{props.title}</h3>
-        <p>{props.price}</p>
-        <p>{props.category}</p>
+        <h3 className='productsTitle'>{props.title}</h3>
+        <p className='productsPrice'>${props.price}</p>
+        <p className='productsCategory'>{props.category}</p>
         <img className = "product-img" src={props.image} alt={props.title}></img>
     </div>
     </Link>
