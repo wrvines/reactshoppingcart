72641ac21c77592705df0b8fc15d06450f329b2e
poopysmack <54606838+poopysmack@users.noreply.github.com>
2022-11-10 17:57:56 -0700

    saving

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import Product from '../Product/Product';
-import './ProductDetail.css';
+import React from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import Product from "../Product/Product";
+import "./ProductDetail.css";
 
 function ProductDetail(props) {
   console.log("need the param");
@@ -10,31 +10,34 @@ function ProductDetail(props) {
   console.log(params.id);
   const [selectedProduct, setSelectedProduct] = React.useState();
   //call the api to get details
-  React.useEffect(
-    ()=>{
-      axios.get(`https://fakestoreapi.com/products/${params.id}`)
-      .then(res =>{
-        console.log(res.data);
-        //save this infor into the state
-        setSelectedProduct(res.data);
-      })
-    }, []
-  )
+  React.useEffect(() => {
+    axios.get(`https://fakestoreapi.com/products/${params.id}`).then((res) => {
+      console.log(res.data);
+      //save this infor into the state
+      setSelectedProduct(res.data);
+    });
+  }, []);
 
   return (
-    <div className='productDetail'>
+    <div className="productDetail">
       <img src={selectedProduct?.image} alt={selectedProduct?.title} />
-      <div className='productInfo'>
+      <div className="productInfo">
         <h3>{selectedProduct?.title}</h3>
-        <p><strong>Description:</strong></p>
+        <p>
+          <strong>Description:</strong>
+        </p>
         <p>{selectedProduct?.description}</p>
-        <p className='productPrice'>${selectedProduct?.price}</p>
+        <p className="productPrice">${selectedProduct?.price}</p>
 
-        <button onClick={()=>props.addProductToCart(selectedProduct)} className='addCart'>Add to Cart</button>
-
+        <button
+          onClick={() => props.addProductToCart(selectedProduct)}
+          className="addCart"
+        >
+          Add to Cart
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default ProductDetail
+export default ProductDetail;
